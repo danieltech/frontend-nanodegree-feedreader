a2a62c7d6f682391e53c06bac7b2a1d8ca093ffe
danieltech <danieltech@gmail.com>
2015-03-17 14:33:21 +0100

    version v0.9

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,30 +31,82 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        function testURL(myfeed) {
+             it('all feeds have an URL', function() {
+                expect(myfeed.url).not.toBe(0);
 
+             });
+        }
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-    });
+        function testName(myfeed){
+            it('ensure each feed has a name', function() {
+                expect(myfeed.name).not.toBe(0);
+            });
+        }
+
+
+        allFeeds.forEach(function(feed) {
+            testURL(feed);
+            testName(feed);
+        });
+
+    }); // describe('RSS Feeds', function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+
+        var menu = $('body').attr('class');
+        var clicked = false;
+
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('menu element hidden by default', function() {
+            expect(menu).not.toEqual('menu');
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        describe('icon is clicked', function() {
+
+            var menu = $('body');
+
+          beforeEach(function() {
+            $('.menu-icon-link').trigger('click');
+          });
+
+          
+          it('menu is not hidden', function() {
+            expect(menu.hasClass('menu-hidden')).toBeFalsy();
+          });
+
+          
+          it('menu is hidden correctly', function() {
+            expect(menu.hasClass('menu-hidden')).toBeTruthy();
+          });
+
+        }); 
+    }); //describe('The menu', function() {
+
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -62,11 +114,35 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         it('at least, a single .feed element exist', function(done) {
+            var menu = $('.feed');
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+            expect(menu.children().length).toBeGreaterThan(0);
+            done();
+         });
+    }); // describe('Initial Entries', function() {
+
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+
+        var before; 
+
+        beforeEach(function(done) {
+            before = $('.feed').find('h2').text();
+            loadFeed(1, done);
+        });
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-}());
+        it('content actually changes', function() {
+            var after = $('.feed').find('h2').text();
+
+            expect(before).not.toBe(after);
+        });
+
+    });
+       
+}()); // function
--- js/app.js
+++ js/app.js
@@ -50,6 +50,7 @@ function loadFeed(id, cb) {
      * is executed.
      */
     feed.load(function(result) {
+        console.log(result);
         if (!result.error) {
             /* If loading the feed did not result in an error,
              * get started making the DOM manipulations required
